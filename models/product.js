e18fad5d13082127f477ba37bc65d649ea174594
Paras Thapar <73242340+paras2707@users.noreply.github.com>
2022-06-24 23:43:16 +0530

    Add files via upload

---
+++ models/product.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: String,
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
